82e62bc3ef61bc92855cd65178cb1c602ffbbc37
Craig Fisk <craigfisk@marmalade.ai>
2020-11-01 17:22:25 -0800

    Initial commit - imported text from 1_5e files in src directory, plus .eslintrc.js etc settings files, from $HOME/video-chat-villalobos, which was a previous runthrough.

--- src/App.vue
+++ src/App.vue
@@ -1,100 +1,12 @@
 <template>
   <div id="app">
-    <Navigation :user="user" @logout="logout" />
-    <router-view
-      :user="user"
-      :rooms="rooms"
-      @logout="logout"
-      @addRoom="addRoom"
-      @deleteRoom="deleteRoom"
-      @checkIn="checkIn"
-    />
+    <Navigation />
+    <router-view />
   </div>
 </template>
 <script>
-import db from './db.js'
-import Firebase from 'firebase'
 import Navigation from '@/components/Navigation'
 export default {
-  name: 'App',
-  data: function() {
-    return {
-      user: null,
-      rooms: []
-    }
-  },
-  methods: {
-    logout: function() {
-      Firebase.auth()
-        .signOut()
-        .then(() => {
-          this.user = null
-          this.$router.push('login')
-        })
-    },
-    addRoom: function(payload) {
-      db.collection('users')
-        .doc(this.user.uid)
-        .collection('rooms')
-        .add({
-          name: payload,
-          createdAt: Firebase.firestore.FieldValue.serverTimestamp()
-        })
-    },
-    deleteRoom: function(payload) {
-      db.collection('users')
-        .doc(this.user.uid)
-        .collection('rooms')
-        .doc(payload)
-        .delete()
-    },
-    checkIn: function(payload) {
-      const roomRef = db
-        .collection('users')
-        .doc(payload.hostID)
-        .collection('rooms')
-        .doc(payload.roomID)
-
-      roomRef.get().then(doc => {
-        if (doc.exists) {
-          roomRef
-            .collection('attendees')
-            .doc(this.user.uid)
-            .set({
-              displayName: payload.displayName,
-              createdAt: Firebase.firestore.FieldValue.serverTimestamp()
-            })
-            .then(() => this.$router.push(`/chat/${payload.hostID}/${payload.roomID}`))
-        }
-      })
-    }
-  },
-  mounted() {
-    Firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.user = user
-        db.collection('users')
-          .doc(this.user.uid)
-          .collection('rooms')
-          .onSnapshot(snapshot => {
-            const snapData = []
-            snapshot.forEach(doc => {
-              snapData.push({
-                id: doc.id,
-                name: doc.data().name
-              })
-            })
-            this.rooms = snapData.sort((a, b) => {
-              if (a.name.toLowerCase() < b.name.toLowerCase()) {
-                return -1
-              } else {
-                return 1
-              }
-            })
-          })
-      }
-    })
-  },
   components: {
     Navigation
   }
--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
-import Rooms from '../views/Rooms.vue'
-import CheckIn from '../views/CheckIn.vue'
-import Chat from '../views/Chat.vue'
 
 Vue.use(VueRouter)
 
@@ -19,30 +15,6 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register
-  },
-  {
-    path: '/rooms',
-    name: 'Rooms',
-    component: Rooms
-  },
-  {
-    path: '/checkin/:hostID/:roomID',
-    name: 'CheckIn',
-    component: CheckIn
-  },
-  {
-    path: '/chat/:hostID/:roomID',
-    name: 'Chat',
-    component: Chat
-  },
-  {
-    path: '*',
-    redirect: '/'
   }
 ]
 
