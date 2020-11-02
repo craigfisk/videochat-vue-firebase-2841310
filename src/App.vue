290429f3e4870e81eac0ffd01a39c53d6620948c
Craig Fisk <craigfisk@marmalade.ai>
2020-11-01 22:35:31 -0800

    Finished section 2 video #3: with gists from the wiki for Home.vue and Navigation.vue, moving the user data to App.vue and accessing as a prop on those two pages.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="app">
-    <Navigation />
-    <router-view />
+    <Navigation :user="user" />
+    <router-view :user="user" />
   </div>
 </template>
 <script>
+import db from './db.js'
 import Navigation from '@/components/Navigation'
 export default {
+  name: 'App',
+  data: function() {
+    return {
+      user: null
+    }
+  },
+  mounted() {
+    db.collection('users')
+      .doc('gV7beTpo7Pp6BembK5te')
+      .get()
+      .then(snapshot => {
+        this.user = snapshot.data().name
+        this.toothpaste = snapshot.data().toothpaste
+      })
+  },
   components: {
     Navigation
   }
