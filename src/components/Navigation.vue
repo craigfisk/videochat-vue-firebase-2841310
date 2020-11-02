33b89af29c34892660966a53046fc763b7aa91bb
Craig Fisk <craigfisk@marmalade.ai>
2020-11-02 15:16:48 -0800

    Finished #2.4 and can now login users that were created in the Firestore database.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,19 +1,19 @@
 <template>
-<div>
-  <nav class="navbar navbar-expand bg-primary navbar-dark">
-    <div class="container-fluid">
-      <router-link class="navbar-brand" to="/"
-        ><span class="h2 pr-1">Video Chat</span>
-        <span class="navbar-text small">
-          : Hi <span class="font-weight-bold text-white">{{ user }}</span></span
-        >
-      </router-link>
-      <div class="navbar-nav ml-auto">
-        <router-link class="nav-item nav-link" to="/login">log in</router-link>
+  <div>
+    <nav class="navbar navbar-expand bg-primary navbar-dark">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/"
+          ><span class="h2 pr-1">Video Chat</span>
+          <span class="navbar-text small">
+            : Hi <span class="font-weight-bold text-white">{{ user }}</span></span
+          >
+        </router-link>
+        <div class="navbar-nav ml-auto">
+          <router-link class="nav-item nav-link" to="/login">log in</router-link>
+        </div>
       </div>
-    </div>
-  </nav>
-</div>
+    </nav>
+  </div>
 </template>
 <script>
 export default {
