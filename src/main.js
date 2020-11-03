86862097e73059dcd73d59277d2d49f17444c648
Craig Fisk <craigfisk@marmalade.ai>
2020-11-03 14:32:32 -0800

    Finished #3 part 3 on deleting rooms.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import {library} from '@fortawesome/fontawesome-svg-core'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash, faVideo, faUser } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faTrash, faVideo, faUser)
 
 Vue.config.productionTip = false
 
