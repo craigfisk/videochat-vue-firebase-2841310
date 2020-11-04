81d437904b2e0dfc1f147ceb8eb4cd5a3fcf27dd
Craig Fisk <craigfisk@marmalade.ai>
2020-11-04 13:08:10 -0800

    Finished #4 video 7 with various webRTC parts.

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -8,12 +8,24 @@
     </div>
     <div class="row" v-if="(user !== null && user.uid == hostID) || attendeeApproved"></div>
     <div v-else>
-      <div class="col-md-8"></div>
+      <div class="col-md-8">
+        <vue-webrtc
+          ref="webrtc"
+          width="100%"
+          :roomId="roomID"
+          v-on:joined-room="doAttendeeJoined"
+          v-on:left-room="doAttendeeLeft"
+        />
+      </div>
       <div class="col-md-4">
-        <button class="btn btn-primary mr-1">
+        <button
+          v-if="!attendeeJoined && attendeeApproved"
+          class="btn btn-primary mr-1"
+          @click="doJoin"
+        >
           Join
         </button>
-        <button type="button" class="btn btn-danger mr-1">
+        <button v-if="attendeeJoined" type="button" class="btn btn-danger mr-1" @click="doLeave">
           Leave
         </button>
         <h4 class="mt-2">Attendees</h4>
@@ -29,7 +41,11 @@
               <font-awesome-icon icon="user"></font-awesome-icon>
             </a>
 
-            <span class="mr-2" title="On Air">
+            <span
+              class="mr-2"
+              :class="[attendee.webRTCID ? 'text-success' : 'text-secondary']"
+              title="On Air"
+            >
               <font-awesome-icon icon="podcast"></font-awesome-icon>
             </span>
             <span
@@ -39,7 +55,7 @@
             >
           </li>
         </ul>
-        <div>
+        <div v-if="user && user.uid == $route.params.hostID">
           <h5 class="mt-4">Pending</h5>
           <ul class="list-unstyled">
             <li class="mb-1" v-for="attendee in attendeesPending" :key="attendee.id">
@@ -61,16 +77,21 @@
                   <font-awesome-icon icon="trash"></font-awesome-icon>
                 </a>
               </span>
-              <span class="pl-1">{{ attendee.displayName }}</span>
+              <span
+                class="pl-1"
+                :class="[attendee.id == user.uid ? 'font-weight-bold text-danger' : '']"
+                >{{ attendee.displayName }}</span
+              >
             </li>
           </ul>
         </div>
       </div>
     </div>
     <div>
-      <p class="lead">
-        Hi <strong class="text-primary font-weight-bold"></strong>, you're currently in the room
-        waiting for the owner of the chat to add you to the meeting. Please wait.
+      <p class="lead" v-if="user">
+        Hi <strong class="text-primary font-weight-bold">{{ user.displayName }} </strong>, you're
+        currently in the room waiting for the owner of the chat to add you to the meeting. Please
+        wait.
       </p>
     </div>
   </div>
@@ -85,6 +106,7 @@ export default {
       attendeesApproved: [],
       attendeesPending: [],
       attendeeApproved: false,
+      attendeeJoined: false,
       hostID: this.$route.params.hostID,
       roomID: this.$route.params.roomID,
       roomName: null,
@@ -129,6 +151,38 @@ export default {
           .doc(attendeeID)
           .delete()
       }
+    },
+    doJoin() {
+      this.$refs.webrtc.join()
+      this.attendeeJoined = true
+    },
+    doLeave() {
+      this.$refs.webrtc.leave()
+      this.attendeeJoined = false
+    },
+    doAttendeeJoined(joinID) {
+      const ref = db
+        .collection('users')
+        .doc(this.hostID)
+        .collection('rooms')
+        .doc(this.roomID)
+        .collection('attendees')
+        .doc(this.user.uid)
+      ref.update({
+        webRTCID: joinID
+      })
+    },
+    doAttendeeLeft(leaveID) {
+      const ref = db
+        .collection('users')
+        .doc(this.hostID)
+        .collection('rooms')
+        .doc(this.roomID)
+        .collection('attendees')
+        .doc(this.user.uid)
+      ref.update({
+        webRTCID: null
+      })
     }
   },
   props: ['user'],
@@ -165,7 +219,8 @@ export default {
           tempApproved.push({
             id: attendeeDocument.id,
             displayName: attendeeDocument.data().displayName,
-            approved: attendeeDocument.data().approved
+            approved: attendeeDocument.data().approved,
+            webRTCID: attendeeDocument.data().webRTCID
           })
         } else {
           if (this.user.uid == attendeeDocument.id) {
@@ -174,7 +229,8 @@ export default {
           tempPending.push({
             id: attendeeDocument.id,
             displayName: attendeeDocument.data().displayName,
-            approved: attendeeDocument.data().approved
+            approved: attendeeDocument.data().approved,
+            webRTCID: attendeeDocument.data().webRTCID
           })
         }
       })
@@ -187,3 +243,19 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+.video-list {
+  margin-bottom: 10px;
+  background: transparent !important;
+}
+.video-item {
+  width: 50%;
+  display: inline-block;
+  background: transparent !important;
+}
+
+.video-item video {
+  width: 100%;
+  height: auto;
+}
+</style>
